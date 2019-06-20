86d16e474b8a5ccb271313451b680f8fcc59948e
Pieter <pieter_dragon3@hotmail.com>
2019-06-20 14:50:51 +0200

    Create master_config.sh
    Script too configure the master server.

---
+++ master_config.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+add-apt-repository "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main"
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common git
+apt-get install -y apache2 munin rsyslog docker-ce salt-master
+git clone https://github.com/Koetjh/musical-disco
+cp -rf ~/musical-disco/munin.conf /etc/munin/
+cp -rf ~/musical-disco/apache24.conf /etc/munin/
+cp -rf ~/musical-disco/rsyslog.conf /etc/
+cp -rf ~/musical-disco/master.conf /etc/salt/master.d/
+mv ~/musical-disco/base.sls /srv/salt/
+mkdir /var/www/munin
+chown munin:munin /var/www/munin
+
+systemctl restart rsyslog
+systemctl restart munin-node
+systemctl restart apache2
+systemctl restart salt-master
+salt '*minion*' state.apply base
